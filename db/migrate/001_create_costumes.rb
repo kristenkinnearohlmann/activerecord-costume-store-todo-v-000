450581d0f6091b8af4a4534c4c12ab11293c7bfc
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-07 19:28:09 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,7 +10,11 @@
 class CreateCostumes < ActiveRecord:Migration[5.2]
   def change
     create_table :costumes do |t|
-
+      t:string :name
+      t:float :price
+      t:integer :size
+      t:text :url
+      t.timestamps
     end
   end
 end
